288d74ec9cb1e724ed2d3ac0f8ca746d73bfbca1
KHaerin <uyancole@gmail.com>
2025-01-09 12:31:45 +0800

    remove google

--- mock_lead/src/main.jsx
+++ mock_lead/src/main.jsx
@@ -4,17 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import {UserProvider} from './UserProvider.jsx'
-import {GoogleOAuthProvider} from '@react-oauth/google'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <GoogleOAuthProvider clientId="206889780183-ga0dkrl4u01p6025in7ctottm2ksj7vt.apps.googleusercontent.com">
       <UserProvider>
       <App />
       </UserProvider>
-    </GoogleOAuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
